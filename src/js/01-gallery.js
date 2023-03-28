61a1dc6810e495a2b10cf7851d5d2013c2961fac
Pavlo Antoniv <pavlivavli@gmail.com>
2023-03-28 21:23:57 +0200

    add js gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryContainerEl = document.querySelector('.gallery');
+const imageEl = createGalleryImg(galleryItems);
+galleryContainerEl.insertAdjacentHTML('beforeend', imageEl);
+
+function createGalleryImg(item) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<ul class="gallery">
+        <li>
+ <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>
+</ul>`;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionType: 'alt',
+});
